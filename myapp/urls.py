788093ea2dfe51a6d943f97c88688c334e52bfad
kavitha <kavinu2010@gmail.com>
2025-01-09 14:00:55 +0100

    following

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import TdList,CreatList
+from .views import TdList,CreatList,DetailList,UpdateList
 
 urlpatterns=[
   path('',TdList.as_view(),name='tdlist'),
+  path('list/<int:pk>/', DetailList.as_view(), name='list'),  
   path('create/',CreatList.as_view(),name='create'),
+  path('list-update/<int:pk>/',UpdateList.as_view(),name='list-update'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.detail import DetailView
 from .models import TodoList
 from django.urls import reverse_lazy
 
@@ -11,8 +12,22 @@ from django.urls import reverse_lazy
 class TdList(ListView):
   model=TodoList
   context_object_name='tdlist'
+
+
+class DetailList(DetailView):
+    model = TodoList
+    context_object_name = 'list'
+
+ 
  
 class CreatList(CreateView):
   model=TodoList
   fields='__all__'
   success_url=reverse_lazy('tdlist')
+
+class UpdateList(UpdateView):
+   model=TodoList
+   fields='__all__'
+   success_url=reverse_lazy('tdlist')
+
+   
